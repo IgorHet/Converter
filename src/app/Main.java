0be2419ccccf93fa09c31f2cdbbb726bcd2bf541
Игорь <flagmr040@gmail.com>
2024-06-15 14:32:19 +0300

    Add Fahrenheit to Celsius conversion

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,16 @@
 package app;
 
 public class Main {
+    private static final double CONV_FACTOR = 5.0 / 9.0;
+
     public static void main(String[] args) {
         System.out.println("App for measures converting.");
+        double fahrenheit = 100;
+        double celsius = convFahrenheitToCelsius(fahrenheit);
+        System.out.println(fahrenheit + " Fahrenheit is " + celsius + " Celsius.");
+    }
+
+    private static double convFahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * CONV_FACTOR;
     }
 }
